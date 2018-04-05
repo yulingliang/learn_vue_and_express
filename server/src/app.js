26e8728592196dbbeb4ae8066e7dd66befa1f31c
yuling <yuling.code@gmail.com>
2018-04-05 15:13:22 +0800

    Created the server code template using express.

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+// import modules into const variable
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// create the endpoints
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
